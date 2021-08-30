65e5f7341638a230edebcb5bc3797541f9eafda5
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-08-30 23:43:40 +0530

    vowel

---
+++ src/VowelConstant.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class VowelConstant {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter character : ");
+		char ch=in.next().charAt(0);
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+			System.out.println(ch + " is vowel");
+		} else
+			System.out.println(ch + " is consonant");
+
+	}
+}
